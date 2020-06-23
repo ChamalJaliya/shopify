7d09020dd4f59b5ef0d972a0be3f62b69251ef60
ChamalJaliya <cjaliya.sln2@gmail.com>
2020-06-23 15:57:55 +0530

    collection-item and collection-preview

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { withRouter } from "react-router-dom";
+
 import "./menu-item.styles.scss"
 // Destructuring title so we can dynamically handle the tilte of each 
 // menu-item exact same thing props.title except pulling the title value 
 // off the props and setting into tile value inside the function
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history,linkUrl , match}) => (
+    <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
         <div className="background-image"
             style={{
                 backgroundImage: `url(${imageUrl})`// String Interpolation
@@ -17,4 +19,5 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
+// giving access to the history location and match
