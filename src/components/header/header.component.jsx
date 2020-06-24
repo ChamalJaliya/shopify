a9d0aca9ffeddcdb15a998b4dec1e7883bcc782c
ChamalJaliya <cjaliya.sln2@gmail.com>
2020-06-24 15:26:23 +0530

    redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./header.styles.scss"
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
 import { ReactComponent as Logo } from "../../assets/4.3 crown.svg.svg";
 import { auth } from "../../firebase/firebase.utils";
 
@@ -27,6 +29,10 @@ const Header = ({ currentUser }) => (
         </div>
 
     </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser:state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
---
+++ src/redux/root-reducer.js
@@ -0,0 +1,7 @@
+// Base Reducer
+ import { combineReducers } from "redux";
+ import userReducer from "./user/user.reducer.jsx";
+
+ export default combineReducers({
+     user:userReducer
+ })
