7498c5ce1b8be86d459e1fd2e629418ce6fd614d
ChamalJaliya <cjaliya.sln2@gmail.com>
2020-06-25 21:50:08 +0530

    checkout

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/11.2 shopping-bag.svg.svg";
 import "./cart-icon.styles.scss"
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
-export default connect(null, mapDispatchToProps)(CartIcon);
+
+// state is always a brand new object
+// this is called a selector
+// const mapStateToProps = ({ cart: { cartItems } }) => ({
+//     itemCount: cartItems.reduce((accumalatedQuantity, cartItem) =>
+//         accumalatedQuantity + cartItem.quantity, 0)
+// })
+
+//caching state with the library selector 
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
+})
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import "./header.styles.scss"
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCartItemsCount, selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selector.js";
 
 import { ReactComponent as Logo } from "../../assets/4.3 crown.svg.svg";
 import { auth } from "../../firebase/firebase.utils";
@@ -37,9 +40,9 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
