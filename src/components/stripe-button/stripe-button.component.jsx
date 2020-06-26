085d6d6a11b02e24c25bf5a3201e63f603c99e17
ChamalJaliya <cjaliya.sln2@gmail.com>
2020-06-26 09:46:03 +0530

    stripe intergration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_51Gy8DFB4kaALjvRor0ohx1eTDQeKa6EBGYDpag0vOQN4ZT0n6teh9tlZ0adM5VlRqh2dd1EdId6xQoTjr5i18YDa00blEP3RvF"
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='SHOPIFY Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import {
     selectCartItems,
     selectCartTotal
@@ -34,6 +34,14 @@ const CheckoutPage = ({ cartItems, total }) => (
             <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         ))}
         <div className='total'>TOTAL: ${total}</div>
+        <div className="test-warning">
+            * Please use following test cart for payments*
+            <br/>
+            4242    4242    4242    4242    -
+            exp - future date   -cvc    any 3 digits
+
+        </div>
+        <StripeCheckoutButton price={total}/>
     </div>
 );
 
